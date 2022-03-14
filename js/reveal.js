fddacaab1a5a7729213f1c7e0b62f53409e14e8a
Jane Dinh <ldinh@ucsd.edu>
2022-03-14 02:05:10 -0700

    added img fripperies to header

--- js/reveal.js
+++ js/reveal.js
@@ -37,3 +37,52 @@ var scrollSrc = "images/logo2.png";
         window.location = href;
       });
     });
+
+    $(".img_a").mouseenter(function(){
+            if ($(this).parent('div').children('div.image').length) {
+                $(this).parent('div').children('div.image').show();
+            } else {
+                var image_name=$(this).data('image');
+                var imageTag='<div class="image" style="position:absolute; margin-top: -10%;">'
+                +'<img src="'+image_name+'" alt="image" height="100""/>'
+                +'</div>';
+                $(this).parent('div').append(imageTag);
+            }
+        });
+
+        $(".img_a").mouseleave(function(){
+            $(this).parent('div').children('div.image').hide();
+        });
+
+    $(".img_b").mouseenter(function(){
+            if ($(this).parent('div').children('div.image').length) {
+                $(this).parent('div').children('div.image').show();
+            } else {
+                var image_name=$(this).data('image');
+                var imageTag='<div class="image" style="position:absolute; margin-top: -10%;">'
+                +'<img src="'+image_name+'" alt="image" height="200" style="filter: grayscale(100%);border-radius:10%;"/>'
+                +'</div>';
+                $(this).parent('div').append(imageTag);
+            }
+        });
+
+        $(".img_b").mouseleave(function(){
+            $(this).parent('div').children('div.image').hide();
+        });
+
+        $(".img_c").mouseenter(function(){
+                if ($(this).parent('div').children('div.image').length) {
+                    $(this).parent('div').children('div.image').show();
+                } else {
+                    var image_name=$(this).data('image');
+                    var imageTag=
+                    '<div class="image" style="position:absolute; margin-left: 5%; margin-top: -10%;">'
+                    +'<img src="'+image_name+'" alt="image" height="200" style="border-radius:10%;"/>'
+                    +'</div>';
+                    $(this).parent('div').append(imageTag);
+                }
+            });
+
+            $(".img_c").mouseleave(function(){
+                $(this).parent('div').children('div.image').hide();
+            });
